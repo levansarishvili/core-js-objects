507ed24d7d277c3a636d85b4c862a9ff790eb300
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-03-16 23:18:14 +0400

    feat: solve some new task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -55,8 +55,12 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const cloneObj = { ...obj };
+  keys.forEach((key) => {
+    delete cloneObj[key];
+  });
+  return cloneObj;
 }
 
 /**
@@ -71,8 +75,15 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  return keys1.every((key) => obj1[key] === obj2[key]);
 }
 
 /**
